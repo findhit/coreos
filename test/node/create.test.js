82dc65e4cf52fe49ce9c4ecbfbf74a983a383715
José Moreira <jmoreira@microdual.com>
2015-05-28 17:00:40 +0100

    accounts related changes

--- test/node/create.test.js
+++ test/node/create.test.js
@@ -4,21 +4,13 @@ var expect = chai.expect;
 var CoreOS = require( '../../' );
 
 describe( "node", function () {
-describe.only( "create", function () {
+describe( "create", function () {
 
 	beforeEach(function () {
 		this.cos = new CoreOS();
-
-		this.cos.accountAdd({
-			id: '1cce3c52-effb-44bf-9b52-276c7b39ea1c',
-			pem: fs.readFileSync( __dirname + '/../resources/1cce3c52-effb-44bf-9b52-276c7b39ea1c.pem', 'utf-8' ) + '',
-		});
-
-		this.cos.accountCurrent( '1cce3c52-effb-44bf-9b52-276c7b39ea1c' );
 	});
 
     it( "should create flawlessly a vm", function () {
-        return this.cos.nodeCreate();
     });
 
 });
